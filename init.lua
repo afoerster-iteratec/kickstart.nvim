c8f7ac92cebcd4adc6f44e6e3d6af9a7085e4783
Adam Förster <adam.foerster@iteratec.com>
2025-02-14 08:52:10 +0100

    set better gruvbox colorscheme and backround to light

--- init.lua
+++ init.lua
@@ -838,11 +838,10 @@ require('lazy').setup({
     end,
   },
   {
-    'Tsuzat/NeoSolarized.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'morhetz/gruvbox',
     config = function()
-      vim.cmd.colorscheme 'NeoSolarized'
+      vim.cmd 'set background=light'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
   -- Highlight todo, notes, etc in comments
